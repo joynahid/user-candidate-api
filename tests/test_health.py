38ee6128b2947ce5d3fb20dd04f5e78bba3c0a51
Nahid <nahid@nahidhq.com>
2023-12-15 01:00:59 +0600

    add unittest for health checker

---
+++ tests/test_health.py
@@ -0,0 +1,14 @@
+import unittest
+from fastapi.testclient import TestClient
+from main import app
+
+class TestAPI(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_health(self):
+        response = self.client.get("/health")
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
